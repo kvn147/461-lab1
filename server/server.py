0997fbf6dff830aa8d51863b98faa073029c8e0f
wuihee <wuihee@gmail.com>
2026-02-05 22:54:16 -0800

    feat(step b1): add step b1

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 
 HOST = "127.0.0.1"
 PORT = 50007
-HEADER_SIZE = 12
+HEADER_LENGTH = 12
 HEADER_FORMAT = "!IIHH"
 FIRST_MESSAGE_SIZE = 24
 
@@ -35,37 +35,39 @@ def run_server() -> None:
 def handle_connection(
     message: bytes, server_socket: socket.socket, client_address: socket._RetAddress
 ):
-    session_state, num_packets, packet_length, udp_port = step_a(
+    session_state, num_packets, data_length, udp_port = step_a1(
         message, server_socket, client_address
     )
 
+    step_b1(session_state, num_packets, data_length, udp_port)
 
-def step_a(
+
+def step_a1(
     message: bytes,
     server_socket: socket.socket,
     client_address: socket._RetAddress,
 ) -> tuple[SessionState, int, int, int]:
     # Parse header.
-    header = message[:HEADER_SIZE]
+    header = message[:HEADER_LENGTH]
     payload_length, incoming_secret, step, student_id = struct.unpack(
         HEADER_FORMAT, header
     )
 
     # Verify header.
-    if len(message) < HEADER_SIZE + payload_length:
-        raise Exception("Incomplete payload")
+    if len(message) < HEADER_LENGTH + payload_length:
+        raise Exception("Incomplete message size")
 
     if incoming_secret != 0:
-        raise Exception("Stage a requires psecret = 0")
+        raise Exception("Incorrect secret")
 
     if step != 1:
-        raise Exception("Invalid step for stage a")
+        raise Exception("Incorrect step")
 
     # Verify payload.
-    payload = message[HEADER_SIZE : HEADER_SIZE + payload_length]
+    payload = message[HEADER_LENGTH : HEADER_LENGTH + payload_length]
 
     if payload != b"hello world\0":
-        raise Exception("Invalid payload")
+        raise Exception("Incorrect payload")
 
     # Update session state to step a1.
     session_state = SessionState(
@@ -76,13 +78,13 @@ def step_a(
 
     # Generate response values.
     num_packets = random.randint(1, 10)
-    packet_length = random.randint(1, 10)
+    data_length = random.randint(1, 10)
     udp_port = random.randint(30000, 65535)
     secret_a = random.randint(0, 1000)
 
     # Build response.
     response_payload = struct.pack(
-        "!IIII", num_packets, packet_length, udp_port, secret_a
+        "!IIII", num_packets, data_length, udp_port, secret_a
     )
     response_header = struct.pack(
         HEADER_FORMAT,
@@ -99,7 +101,85 @@ def step_a(
     session_state.step = 1
     session_state.secret = secret_a
 
-    return session_state, num_packets, packet_length, udp_port
+    return session_state, num_packets, data_length, udp_port
+
+
+def step_b1(
+    session_state: SessionState, num_packets: int, data_length: int, udp_port: int
+):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.bind((HOST, udp_port))
+        server_socket.settimeout(3)
+
+        packet_id_length = 4
+        payload_length_no_padding = packet_id_length + data_length
+        padding = (-payload_length_no_padding) % 4
+
+        expected_packet = 0
+        skipped_ack = False
+
+        while expected_packet < num_packets:
+            # Receive next packet.
+            message, client_address = server_socket.recvfrom(1024)
+
+            # Parse header.
+            header = message[:HEADER_LENGTH]
+            payload_length, incoming_secret, step, student_id = struct.unpack(
+                HEADER_FORMAT, header
+            )
+
+            # Verify header.
+            if payload_length != packet_id_length + data_length:
+                raise Exception("Incorrect payload length")
+
+            if len(message) != HEADER_LENGTH + payload_length + padding:
+                raise Exception("Incorrect payload length with padding")
+
+            if incoming_secret != session_state.secret:
+                raise Exception("Incorrect secret")
+
+            if step != session_state.step:
+                raise Exception("Incorrect step")
+
+            if student_id != session_state.student_id:
+                raise Exception("Incorrect student ID")
+
+            # Verify packet ID.
+            payload = message[HEADER_LENGTH : HEADER_LENGTH + payload_length]
+            (packet_id,) = struct.unpack("!I", payload[:packet_id_length])
+
+            if packet_id != expected_packet:
+                raise Exception("Incorrect packet ID")
+
+            # Verify data.
+            data = payload[packet_id_length : packet_id_length + data_length]
+
+            if data != b"\x00" * data_length:
+                raise Exception("Incorrect data")
+
+            # ACK logic.
+            should_ack = random.choice([True, False])
+
+            if not skipped_ack and should_ack:
+                should_ack = False
+                skipped_ack = True
+
+            if should_ack:
+                ack_payload = struct.pack("!I", packet_id)
+                ack_header = struct.pack(
+                    HEADER_FORMAT,
+                    len(ack_payload),
+                    session_state.secret,
+                    session_state.step,
+                    session_state.student_id,
+                )
+
+                server_socket.sendto(ack_header + ack_payload, client_address)
+
+                expected_packet += 1
+
+        # Update session state to b2.
+        session_state.step = 2
 
 
 if __name__ == "__main__":
